6ad4485977e34afb7f6d97bb3f2cd13a2f0467cd
Tim Gymnich <tg908@icloud.com>
2020-06-28 17:00:53 +0200

    Initial Commit

---
+++ Shared/ShinyAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShinyAuthApp.swift
+//  Shared
+//
+//  Created by Tim Gymnich on 28.6.20.
+//
+
+import SwiftUI
+
+@main
+struct ShinyAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
